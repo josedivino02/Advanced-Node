56bf1bc987ee35bc52fa032dc6a454309ddd4692
José Divino <josedivinooficial02@gmail.com>
2023-05-29 12:26:09 -0300

    isolate SUT creeation

--- tests/external/facebook-api.test.ts
+++ tests/external/facebook-api.test.ts
@@ -3,14 +3,19 @@ import { AxiosHttpClient } from '@/infra/http';
 import { env } from '@/main/config/env';
 
 describe('Facebook APi Integration Tests', () => {
-  it('should return a Facebook User if token is valid', async () => {
-    const axiosClient = new AxiosHttpClient();
-    const sut = new FacebookApi(
+  let axiosClient: AxiosHttpClient;
+  let sut: FacebookApi;
+
+  beforeEach(() => {
+    axiosClient = new AxiosHttpClient();
+    sut = new FacebookApi(
       axiosClient,
       env.facebookApi.clientId,
       env.facebookApi.clientSecret
     );
+  });
 
+  it('should return a Facebook User if token is valid', async () => {
     const fbUser = await sut.loadUser({ token: '' });
 
     expect(fbUser).toEqual({
@@ -21,13 +26,6 @@ describe('Facebook APi Integration Tests', () => {
   });
 
   it('should return undefined if token is invalid', async () => {
-    const axiosClient = new AxiosHttpClient();
-    const sut = new FacebookApi(
-      axiosClient,
-      env.facebookApi.clientId,
-      env.facebookApi.clientSecret
-    );
-
     const fbUser = await sut.loadUser({ token: 'invalid' });
 
     expect(fbUser).toBeUndefined();
