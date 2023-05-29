4d1128deeb23ed354d9d84ce18fc948dd7126152
José Divino <josedivinooficial02@gmail.com>
2023-05-29 14:17:55 -0300

    add Facebook Login factories

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -5,4 +5,5 @@ export const env = {
       process.env.FB_CLIENT_SECRET ?? '9210dceb724ad8e5610e7d14a91383e6',
   },
   port: process.env.PORT ?? 8080,
+  jwtSecret: process.env.JWT_SECRET ?? '38cdxhnf30',
 };
