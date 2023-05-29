fd34a8a61ad8a12833d99b4cf41a051ffcfc39ec
José Divino <josedivinooficial02@gmail.com>
2023-05-29 15:01:16 -0300

    transform ExpressRouter to a function

--- src/infra/http/express-router.ts
+++ src/infra/http/express-router.ts
@@ -1,11 +1,9 @@
 import { Controller } from '@/application/controllers';
-import { Request, Response } from 'express';
+import { RequestHandler } from 'express';
 
-export class ExpressRouter {
-  constructor(private readonly controller: Controller) {}
-
-  async adapt(req: Request, res: Response): Promise<void> {
-    const httpResponse = await this.controller.handle({ ...req.body });
+export const adaptExpressRoute = (controller: Controller): RequestHandler => {
+  return async (req, res) => {
+    const httpResponse = await controller.handle({ ...req.body });
     if (httpResponse.statusCode === 200) {
       res.status(200).json(httpResponse);
     } else {
@@ -13,5 +11,25 @@ export class ExpressRouter {
         .status(httpResponse.statusCode)
         .json({ error: httpResponse.data.message });
     }
-  }
-}
+  };
+};
+
+// export const adaptExpressRoute = (controller: Controller): RequestHandler => {
+//   return (req, res) => {
+//     controller
+//       .handle({ ...req.body })
+//       .then((httpResponse) => {
+//         if (httpResponse.statusCode === 200) {
+//           res.status(200).json(httpResponse);
+//         } else {
+//           res
+//             .status(httpResponse.statusCode)
+//             .json({ error: httpResponse.data.message });
+//         }
+//       })
+//       .catch((error) => {
+//         console.error(error);
+//         res.status(500).json({ error: error.data.message });
+//       });
+//   };
+// };
--- tests/infra/http/express-router.spec.ts
+++ tests/infra/http/express-router.spec.ts
@@ -1,14 +1,15 @@
 import { Controller } from '@/application/controllers';
-import { ExpressRouter } from '@/infra/http';
+import { adaptExpressRoute } from '@/infra/http';
 import { getMockReq, getMockRes } from '@jest-mock/express';
-import { Request, Response } from 'express';
+import { NextFunction, Request, RequestHandler, Response } from 'express';
 import { MockProxy, mock } from 'jest-mock-extended';
 
 describe('ExpressRouter', () => {
   let req: Request;
   let res: Response;
+  let next: NextFunction;
   let controller: MockProxy<Controller>;
-  let sut: ExpressRouter;
+  let sut: RequestHandler;
 
   beforeEach(() => {
     req = getMockReq({
@@ -17,16 +18,17 @@ describe('ExpressRouter', () => {
       },
     });
     res = getMockRes().res;
+    next = getMockRes().next;
     controller = mock();
     controller.handle.mockResolvedValue({
       statusCode: 200,
       data: { data: 'any_data' },
     });
-    sut = new ExpressRouter(controller);
+    sut = adaptExpressRoute(controller);
   });
 
   it('should call handle with correct request', async () => {
-    await sut.adapt(req, res);
+    await sut(req, res, next);
 
     expect(controller.handle).toHaveBeenCalledWith({ any: 'any' });
     expect(controller.handle).toHaveBeenCalledTimes(1);
@@ -35,14 +37,14 @@ describe('ExpressRouter', () => {
   it('should call handle with empty request', async () => {
     const req = getMockReq();
 
-    await sut.adapt(req, res);
+    await sut(req, res, next);
 
     expect(controller.handle).toHaveBeenCalledWith({});
     expect(controller.handle).toHaveBeenCalledTimes(1);
   });
 
   it('should respond with 200 and valid data', async () => {
-    await sut.adapt(req, res);
+    await sut(req, res, next);
 
     expect(res.status).toHaveBeenCalledWith(200);
     expect(res.status).toHaveBeenCalledTimes(1);
@@ -59,7 +61,7 @@ describe('ExpressRouter', () => {
       data: new Error('any_error'),
     });
 
-    await sut.adapt(req, res);
+    await sut(req, res, next);
 
     expect(res.status).toHaveBeenCalledWith(400);
     expect(res.status).toHaveBeenCalledTimes(1);
@@ -73,7 +75,7 @@ describe('ExpressRouter', () => {
       data: new Error('any_error'),
     });
 
-    await sut.adapt(req, res);
+    await sut(req, res, next);
 
     expect(res.status).toHaveBeenCalledWith(500);
     expect(res.status).toHaveBeenCalledTimes(1);
