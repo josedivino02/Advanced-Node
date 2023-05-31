747854f7442c26c42401573b31c7c649e5003dda
José Divino <josedivinooficial02@gmail.com>
2023-05-31 14:55:10 -0300

    ensure JwtTokenHandler calls verify with correct params

--- tests/infra/crypto/jwt-token-handler.spec.ts
+++ tests/infra/crypto/jwt-token-handler.spec.ts
@@ -36,6 +36,7 @@ describe('JwtTokenHandler', () => {
       expect(fakeJwt.sign).toHaveBeenCalledWith({ key }, secret, {
         expiresIn: 1,
       });
+      expect(fakeJwt.sign).toHaveBeenCalledTimes(1);
     });
 
     it('should return a token', async () => {
@@ -58,6 +59,21 @@ describe('JwtTokenHandler', () => {
       });
 
       await expect(promise).rejects.toThrow(new Error('token_error'));
+    });
+  });
+
+  describe('validateToken', () => {
+    let token: string;
+
+    beforeAll(() => {
+      token = 'any_token';
+    });
+
+    it('should call verify with correct params', async () => {
+      await sut.validateToken({ token });
+
+      expect(fakeJwt.verify).toHaveBeenCalledWith(token, secret);
+      expect(fakeJwt.verify).toHaveBeenCalledTimes(1);
     });
   });
 });
