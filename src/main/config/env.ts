8dee057568f6deccf23a32d2bb7114c76c1dead1
José Divino <josedivinooficial02@gmail.com>
2023-05-31 16:18:32 -0300

    ensure ExpressMiddleware class handle with correct request

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,9 +1,8 @@
 export const env = {
   facebookApi: {
-    clientId: process.env.FB_CLIENT_ID ?? '1390544478456133',
-    clientSecret:
-      process.env.FB_CLIENT_SECRET ?? '9210dceb724ad8e5610e7d14a91383e6',
+    clientId: process.env.FB_CLIENT_ID ?? '',
+    clientSecret: process.env.FB_CLIENT_SECRET ?? '',
   },
   port: process.env.PORT ?? 8080,
-  jwtSecret: process.env.JWT_SECRET ?? '38cdxhnf30',
+  jwtSecret: process.env.JWT_SECRET ?? '',
 };
