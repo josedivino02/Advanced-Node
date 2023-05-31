d751f19474a59b00040e8d4f6d7955ad1d73ed50
José Divino <josedivinooficial02@gmail.com>
2023-05-31 17:27:18 -0300

    remove anti patterns middle man

--- src/application/middlewares/authentication.ts
+++ src/application/middlewares/authentication.ts
@@ -1,9 +1,10 @@
 import { HttpResponse, forbidden, ok } from '@/application/helpers';
 import { Middleware } from '@/application/middlewares';
 import { RequiredStringValidator } from '@/application/validation';
-import { Authorize } from '@/domain/usecases';
+
 type HttpRequest = { authorization: string };
 type Model = Error | { userId: string };
+type Authorize = (params: { token: string }) => Promise<string>;
 
 export class AuthenticationMiddleware implements Middleware {
   constructor(private readonly authorize: Authorize) {}
