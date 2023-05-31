ef7f671d8027298190eda26813123c3705c72a7f
José Divino <josedivinooficial02@gmail.com>
2023-05-31 14:43:25 -0300

    rename JwtTokenGenerator to JwtTokenHandler

--- src/main/factories/usecases/facebook-authentication.ts
+++ src/main/factories/usecases/facebook-authentication.ts
@@ -3,13 +3,13 @@ import {
   setupFacebookAuthentication,
 } from '@/domain/usecases';
 import { makeFacebookApi } from '@/main/factories/apis';
-import { makeJwtTokenGenerator } from '@/main/factories/crypto';
+import { makeJwtTokenHandler } from '@/main/factories/crypto';
 import { makePgUserAccountRepo } from '@/main/factories/repos';
 
 export const makeFacebookAuthentication = (): FacebookAuthentication => {
   return setupFacebookAuthentication(
     makeFacebookApi(),
     makePgUserAccountRepo(),
-    makeJwtTokenGenerator()
+    makeJwtTokenHandler()
   );
 };
