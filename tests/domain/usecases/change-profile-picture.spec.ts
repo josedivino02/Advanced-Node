3e80678b99dc2486ece6b209d27f32974b28da51
José Divino <josedivinooficial02@gmail.com>
2023-06-01 18:20:11 -0300

    ensure ChangeProfilePicture returns correct dat on success

--- tests/domain/usecases/change-profile-picture.spec.ts
+++ tests/domain/usecases/change-profile-picture.spec.ts
@@ -67,4 +67,20 @@ describe('ChangeProfilePicture', () => {
 
     expect(userProfileRepo.load).not.toHaveBeenCalled();
   });
+
+  it('should return correct data success', async () => {
+    jest.mocked(UserProfile).mockImplementation((id) => ({
+      setPicture: jest.fn(),
+      id: 'any_id',
+      pictureUrl: 'any_url',
+      initials: 'any_initials',
+    }));
+
+    const result = await sut({ id: 'any_id', file });
+
+    expect(result).toMatchObject({
+      pictureUrl: 'any_url',
+      initials: 'any_initials',
+    });
+  });
 });
