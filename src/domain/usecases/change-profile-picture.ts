6bb392d4577ac3b5de1ddbddd39f1dfe8c89f46a
José Divino <josedivinooficial02@gmail.com>
2023-06-01 13:51:55 -0300

    ensure ChangeProfilePicture calls SaveUserPicture with correct input when file is undefined

--- src/domain/usecases/change-profile-picture.ts
+++ src/domain/usecases/change-profile-picture.ts
@@ -13,12 +13,12 @@ export type ChangeProfilePicture = (input: Input) => Promise<void>;
 export const setupChangeProfilePicture: Setup =
   (fileStorage, crypto, userProfileRepo) =>
   async ({ id, file }) => {
+    let pictureUrl: string | undefined;
     if (file !== undefined) {
-      const pictureUrl = await fileStorage.upload({
+      pictureUrl = await fileStorage.upload({
         file,
         key: crypto.uuid({ key: id }),
       });
-
-      await userProfileRepo.savePicture({ pictureUrl });
     }
+    await userProfileRepo.savePicture({ pictureUrl });
   };
