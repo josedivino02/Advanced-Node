59bbdd01cb5c54d52055881e71f7aa1fdf2d82dd
José Divino <josedivinooficial02@gmail.com>
2023-06-02 17:43:30 -0300

    ensure SavePictureController return 400 if file size is more than 5MB

--- tests/application/controllers/save-picture.spec.ts
+++ tests/application/controllers/save-picture.spec.ts
@@ -14,6 +14,8 @@ class SavePictureController {
       return badRequest(new RequiredFieldError('file'));
     if (!['image/png', 'image/jpg', 'image/jpeg'].includes(file.mimeType))
       return badRequest(new InvalidMimeTypeError(['png', 'jpeg']));
+    if (file.buffer.length > 5 * 1024 * 1024)
+      return badRequest(new MaxFileSizeError(5));
   }
 }
 
@@ -21,6 +23,13 @@ class InvalidMimeTypeError extends Error {
   constructor(allowed: string[]) {
     super(`Unsupported type. Allowed types: ${allowed.join(', ')}`);
     this.name = 'InvalidMimeTypeError';
+  }
+}
+
+class MaxFileSizeError extends Error {
+  constructor(maxSizeInMb: number) {
+    super(`File upload limit is ${maxSizeInMb}`);
+    this.name = 'MaxFileSizeError';
   }
 }
 
@@ -108,6 +117,18 @@ describe('SavePictureController', () => {
     expect(httpResponse).not.toEqual({
       statusCode: 400,
       data: new InvalidMimeTypeError(['png', 'jpeg']),
+    });
+  });
+
+  it('should return 400 if file size is bigger than 5MB', async () => {
+    const invalidBuffer = Buffer.from(new ArrayBuffer(6 * 1024 * 1024));
+    const httpResponse = await sut.handle({
+      file: { buffer: invalidBuffer, mimeType },
+    });
+
+    expect(httpResponse).toEqual({
+      statusCode: 400,
+      data: new MaxFileSizeError(5),
     });
   });
 });
