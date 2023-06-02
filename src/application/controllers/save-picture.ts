5c5f96c7a4bb2fd31b1b7fb27c3eaa2283862fe5
José Divino <josedivinooficial02@gmail.com>
2023-06-02 18:04:45 -0300

    ensure SavePictureController extends Controller1~

--- src/application/controllers/save-picture.ts
+++ src/application/controllers/save-picture.ts
@@ -1,3 +1,4 @@
+import { Controller } from '@/application/controllers';
 import {
   InvalidMimeTypeError,
   MaxFileSizeError,
@@ -12,10 +13,15 @@ type HttpRequest = {
 };
 type Model = Error | { initials?: string; pictureUrl?: string };
 
-export class SavePictureController {
-  constructor(private readonly changeProfilePicture: ChangeProfilePicture) {}
+export class SavePictureController extends Controller {
+  constructor(private readonly changeProfilePicture: ChangeProfilePicture) {
+    super();
+  }
 
-  async handle({ file, userId }: HttpRequest): Promise<HttpResponse<Model>> {
+  override async perform({
+    file,
+    userId,
+  }: HttpRequest): Promise<HttpResponse<Model>> {
     if (file === undefined || file === null)
       return badRequest(new RequiredFieldError('file'));
     if (file.buffer.length === 0)
