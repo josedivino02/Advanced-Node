44fb86ac581ff33904f26579f34de905358ace19
José Divino <josedivinooficial02@gmail.com>
2023-06-02 18:51:33 -0300

    add RequiredBuffer Validator

--- src/application/validation/required.ts
+++ src/application/validation/required.ts
@@ -1,8 +1,10 @@
 import { RequiredFieldError } from '@/application/errors';
 import { Validator } from '@/application/validation';
 
+type TypeValue = string | Buffer;
+
 export class Required implements Validator {
-  constructor(readonly value: string, readonly fieldName?: string) {}
+  constructor(readonly value: TypeValue, readonly fieldName?: string) {}
 
   validate(): Error | undefined {
     if (this.value === null || this.value === undefined) {
@@ -21,6 +23,21 @@ export class RequiredString extends Required {
 
   override validate(): Error | undefined {
     if (super.validate() !== undefined || this.value === '') {
+      return new RequiredFieldError(this.fieldName);
+    }
+  }
+}
+
+export class RequiredBuffer extends Required {
+  constructor(
+    override readonly value: Buffer,
+    override readonly fieldName?: string
+  ) {
+    super(value, fieldName);
+  }
+
+  override validate(): Error | undefined {
+    if (super.validate() !== undefined || this.value.length === 0) {
       return new RequiredFieldError(this.fieldName);
     }
   }
--- tests/application/validation/required.spec.ts
+++ tests/application/validation/required.spec.ts
@@ -1,5 +1,9 @@
 import { RequiredFieldError } from '@/application/errors';
-import { Required, RequiredString } from '@/application/validation';
+import {
+  Required,
+  RequiredBuffer,
+  RequiredString,
+} from '@/application/validation';
 
 describe('Required', () => {
   it('Should return RequiredFieldError if value is null', () => {
@@ -44,6 +48,30 @@ describe('RequiredString', () => {
 
   it('Should return undefined if value is not empty', () => {
     const sut = new RequiredString('any_value', 'any_field');
+
+    const error = sut.validate();
+
+    expect(error).toBeUndefined();
+  });
+});
+
+describe('RequiredBuffer', () => {
+  it('Should extend Required', () => {
+    const sut = new RequiredBuffer(Buffer.from(''));
+
+    expect(sut).toBeInstanceOf(Required);
+  });
+
+  it('Should return RequiredFieldError if value is empty', () => {
+    const sut = new RequiredBuffer(Buffer.from(''));
+
+    const error = sut.validate();
+
+    expect(error).toEqual(new RequiredFieldError());
+  });
+
+  it('Should return undefined if value is not empty', () => {
+    const sut = new RequiredBuffer(Buffer.from('amy_buffer'));
 
     const error = sut.validate();
 
