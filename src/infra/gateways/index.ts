a7de0d79c6cbbdc7398dace881ab04099d7c8d13
José Divino <josedivinooficial02@gmail.com>
2023-06-02 12:12:22 -0300

    move AwsS3FileStorage to its own files

--- src/infra/gateways/index.ts
+++ src/infra/gateways/index.ts
@@ -3,4 +3,5 @@ export * from '../gateways/client';
 export * from '../gateways/jwt-token-handler';
 export * from '../gateways/unique-id';
 export * from '../gateways/uuid-handler';
+export * from './aws-s3-file-storage';
 export * from './facebook-api';
--- tests/infra/gateways/aws-s3-file-storage.spec.ts
+++ tests/infra/gateways/aws-s3-file-storage.spec.ts
@@ -1,35 +1,7 @@
-import { UploadFile } from '@/domain/contracts/gateways';
+import { AwsS3FileStorage } from '@/infra/gateways';
 import { S3, config } from 'aws-sdk';
 
 jest.mock('aws-sdk');
-
-class AwsS3FileStorage implements UploadFile {
-  constructor(
-    accessKey: string,
-    secret: string,
-    private readonly bucket: string
-  ) {
-    config.update({
-      credentials: {
-        accessKeyId: accessKey,
-        secretAccessKey: secret,
-      },
-    });
-  }
-
-  async upload({ file, key }: UploadFile.Input): Promise<UploadFile.Output> {
-    const s3 = new S3();
-    await s3
-      .putObject({
-        Bucket: this.bucket,
-        Key: key,
-        Body: file,
-        ACL: 'public-read',
-      })
-      .promise();
-    return `https://${this.bucket}.s3.amazonaws.com/${encodeURIComponent(key)}`;
-  }
-}
 
 describe('AwsS3FileStorage', () => {
   let accessKey: string;
