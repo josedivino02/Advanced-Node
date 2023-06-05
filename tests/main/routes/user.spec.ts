1fe49e1b175a1a7250b412fba950fa8886cebfc3
José Divino <josedivinooficial02@gmail.com>
2023-06-05 12:02:08 -0300

    ensure PUT /users/picture returns 403 is no authorization header is present

--- tests/main/routes/user.spec.ts
+++ tests/main/routes/user.spec.ts
@@ -8,24 +8,24 @@ import request from 'supertest';
 import { Repository, getConnection, getRepository } from 'typeorm';
 
 describe('User Routes', () => {
+  let backup: IBackup;
+  let pgUserRepo: Repository<PgUser>;
+
+  beforeAll(async () => {
+    const db = await makeFakeDb([PgUser]);
+    backup = db.backup();
+    pgUserRepo = getRepository(PgUser);
+  });
+
+  afterAll(async () => {
+    await getConnection().close();
+  });
+
+  beforeEach(() => {
+    backup.restore();
+  });
+
   describe('DELETE /users/picture', () => {
-    let backup: IBackup;
-    let pgUserRepo: Repository<PgUser>;
-
-    beforeAll(async () => {
-      const db = await makeFakeDb([PgUser]);
-      backup = db.backup();
-      pgUserRepo = getRepository(PgUser);
-    });
-
-    afterAll(async () => {
-      await getConnection().close();
-    });
-
-    beforeEach(() => {
-      backup.restore();
-    });
-
     it('should return 403 if no authorization header is present', async () => {
       const { status } = await request(app).delete('/api/users/picture');
 
@@ -45,6 +45,14 @@ describe('User Routes', () => {
 
       expect(status).toBe(200);
       expect(body).toEqual({ pictureUrl: undefined, initials: 'JD' });
+    });
+  });
+
+  describe('PUT /users/picture', () => {
+    it('should return 403 if no authorization header is present', async () => {
+      const { status } = await request(app).put('/api/users/picture');
+
+      expect(status).toBe(403);
     });
   });
 });
