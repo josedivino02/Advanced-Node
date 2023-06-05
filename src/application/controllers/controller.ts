09161d78ee4faf9afff2c83e622f60f879a0e098
José Divino <josedivinooficial02@gmail.com>
2023-06-05 18:33:48 -0300

    change serverError to receive unknown instead of Error

--- src/application/controllers/controller.ts
+++ src/application/controllers/controller.ts
@@ -17,7 +17,7 @@ export abstract class Controller {
     try {
       return await this.perform(httpRequest);
     } catch (error) {
-      return serverError(error as Error);
+      return serverError(error);
     }
   }
 
