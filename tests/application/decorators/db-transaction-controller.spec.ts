da1fe91b13bb2d34036be14ef2de6d1f03b6d607
José Divino <josedivinooficial02@gmail.com>
2023-06-05 19:39:54 -0300

    ensure DbTransactionController extends Controller

--- tests/application/decorators/db-transaction-controller.spec.ts
+++ tests/application/decorators/db-transaction-controller.spec.ts
@@ -2,13 +2,15 @@ import { Controller } from '@/application/controllers';
 import { HttpResponse } from '@/application/helpers';
 import { mock, MockProxy } from 'jest-mock-extended';
 
-class DbTransactionController {
+class DbTransactionController extends Controller {
   constructor(
     private readonly decoratee: Controller,
     private readonly db: DbTransaction
-  ) {}
+  ) {
+    super();
+  }
 
-  async perform(httpRequest: any): Promise<HttpResponse | undefined> {
+  async perform(httpRequest: any): Promise<HttpResponse> {
     await this.db.openTransaction();
     try {
       const httpResponse = await this.decoratee.perform(httpRequest);
@@ -43,6 +45,10 @@ describe('DbTransactionController', () => {
 
   beforeEach(() => {
     sut = new DbTransactionController(decoratee, db);
+  });
+
+  it('should extends Controller', async () => {
+    expect(sut).toBeInstanceOf(Controller);
   });
 
   it('should open transaction', async () => {
